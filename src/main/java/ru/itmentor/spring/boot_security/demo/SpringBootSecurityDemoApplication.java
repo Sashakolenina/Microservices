41650165dbf51ebc2509bd112d2272230c7d5b94
Sashakolenina <Sasha_kolenina@mail.ru>
2025-06-30 23:23:45 +0300

    feature/Spring_Rest_template

--- src/main/java/ru/itmentor/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -2,12 +2,16 @@ package ru.itmentor.spring.boot_security.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import ru.itmentor.spring.boot_security.demo.service.UserService;
 
 @SpringBootApplication
 public class SpringBootSecurityDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+		ApplicationContext context=SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+		UserService userService = context.getBean(UserService.class);
+		userService.runMethods();
 	}
 
 }
